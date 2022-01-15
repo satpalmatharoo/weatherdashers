213ecc7de59084a5c97531fedabf7b53fb6da0f0
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-01-15 19:55:36 +0000

    add bootstrap

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,9 +5,18 @@ function getApiEndpoint(city) {
     return `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 }   
 
-//
+//make request
 function makeRequest(endpoint) {
     return fetch(endpoint).then(function (res){
         return res.json();
     });
 }
+
+
+var city = "Birmingham";
+
+var endpoint = generateEndpoint(city);
+
+makeRequest(endpoint).then(function(weatherData){
+
+})
