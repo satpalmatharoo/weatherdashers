0316ae52ae9ccd82a6782d7cd8aa0f091c0b3485
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-01-16 01:22:09 +0000

    api function

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,22 +1,56 @@
 var apiKey= "0d7986ede662de9f77df50487eb98a46";
-
+var workingapikey ='https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=0d7986ede662de9f77df50487eb98a46'
 //url endpoint for city
 function getApiEndpoint(city) {
-    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    
+    var endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    return endpoint;
 }   
 
-//make request
-function makeRequest(endpoint) {
-    return fetch(endpoint).then(function (res){
-        return res.json();
-    });
+// function to test api is working
+function testData(request) {
+    request = workingapikey;
+    console.log( request)
+
+//request api info
+    fetch(request).then(function (res)
+{
+    if (res.ok) { 
+    res
+    .json()
+        // console.log(test)
+        .then(function (weatherData) {
+            DataTest(weatherData);
+        })
+        
+        
+    } 
+})
+
+function DataTest(weatherData) {
+    temp = weatherData.main.temp;
+    console.log('big pooopy weather scum' + temp)
 }
 
 
-var city = "Birmingham";
+    
+    S
+} 
 
-var endpoint = generateEndpoint(city);
+testData();
 
-makeRequest(endpoint).then(function(weatherData){
+//make request
+// function makeRequest(endpoint) {
+//     return fetch(endpoint).then(function (res){
+//         return res.json();
+//     });
+// }
 
-})
+
+// var city = "Birmingham";
+
+// var endpoint = generateEndpoint(city);
+
+// makeRequest(endpoint).then(function(weatherData){
+
+// })
