183470b8b6504246b17767a7f287e815d6e49989
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-01-25 02:32:15 +0000

    set up 2nd working api key

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,14 @@
-var apiKey= " ";
+var apiKey= "0d7986ede662de9f77df50487eb98a46 ";
 var workingapikey ='https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=0d7986ede662de9f77df50487eb98a46'
+// var citySelectionForm =$("#city selection form");
+
+// citySelectionForm.on("submit", function(event) {
+//     event.preventDefault();
+    
+//     console.log("form submitted");
+// });
+ $("#section-heading").text("Hello fromJquery");
+//you goose
 
 //url endpoint for city
 function getApiEndpoint(city) {
@@ -30,27 +39,51 @@ function testData(request) {
 
 function DataTest(weatherData) {
     temp = weatherData.main.temp;
+    humidity = weatherData.main.humidity;
+    wind = weatherData.wind.speed;
+    icon =weatherData.weather[0].icon;
+    console.log ("weatherData" + JSON.stringify(weatherData))
     console.log('big pooopy weather scum' + temp)
+    console.log(humidity + "big humidity pooopy weather scum")
+    console.log("big poopy wind speed"+wind)
+    console.log("big poopy icon"+ icon)
+
+    function getweekweather() {
+       var lat = weatherData.coord.lat
+       var long = weatherData.coord.lon
+        
+        var apiTwo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`;
+        fetch(apiTwo).then(function (res) {
+            if (res.ok) {
+                res.json().then(function (weeklyData) {
+                    populateWeeklyData(weeklyData);
+
+                });
+            }
+        });
+    }
+    getweekweather();
 }
 
 
-    
+
 } 
+
+
 
 testData();
 
-//make request
-// function makeRequest(endpoint) {
+// function makeRequest(endpoint) 
 //     return fetch(endpoint).then(function (res){
 //         return res.json();
 //     });
 // }
 
 
-// var city = "Birmingham";
+
 
 // var endpoint = generateEndpoint(city);
 
 // makeRequest(endpoint).then(function(weatherData){
 
-// })
+// };
