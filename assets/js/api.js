4b970b623a1ba87e11f3204447e7449f4984e3d2
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-01-26 16:28:21 +0000

    api request html rendering

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,89 +1,105 @@
-var apiKey= "0d7986ede662de9f77df50487eb98a46 ";
-var workingapikey ='https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=0d7986ede662de9f77df50487eb98a46'
-// var citySelectionForm =$("#city selection form");
+var apiKey = "0d7986ede662de9f77df50487eb98a46 ";
 
-// citySelectionForm.on("submit", function(event) {
-//     event.preventDefault();
-    
-//     console.log("form submitted");
-// });
- $("#section-heading").text("Hello fromJquery");
-//you goose
+var searchFormEl = document.querySelector("form");
+var citySearchEl = document.querySelector("#city-selection");
+// puts searched city into endpoint
+function citySearch(event) {
+    event.preventDefault();
+    const searchedCity = citySearchEl.value;
+    console.log("searchedCity" + searchedCity);
+    // call api function that gets the end point 
+    getApiEndpoint(searchedCity);
+  }
 
-//url endpoint for city
+//   creates api for the resquest but does not make the request
 function getApiEndpoint(city) {
-    
-    var endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    return endpoint;
-}   
+  var endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  console.log("getApi function " + endpoint);
+  console.log("this is the city in endpoint " +city)
+  var htmlcity = document.getElementById("htmlCity")
+  htmlcity.innerText = city;
 
-// function to test api is working
-function testData(request) {
-    request = workingapikey;
-    console.log( request)
 
-//request api info
-    fetch(request).then(function (res)
-{
-    if (res.ok) { 
-    res
-    .json()
-        // console.log(test)
+//   initiates getData func that makes the request
+  getData(endpoint);
+}
+// makes api request
+function getData(request) {
+ 
+  console.log("getdata function " + request);
+  
+
+  //request api info
+  fetch(request).then(function (res) {
+    if (res.ok) {
+      res
+        .json()
+
         .then(function (weatherData) {
-            DataTest(weatherData);
-        })
-        
-        
-    } 
-})
-
-function DataTest(weatherData) {
+            // use weather data in the populate ui function after req made. 
+          populateUI(weatherData);
+        });
+    }
+  });
+}
+// displays weather data to front end 
+function populateUI(weatherData) {
+   
     temp = weatherData.main.temp;
     humidity = weatherData.main.humidity;
     wind = weatherData.wind.speed;
-    icon =weatherData.weather[0].icon;
-    console.log ("weatherData" + JSON.stringify(weatherData))
-    console.log('big pooopy weather scum' + temp)
-    console.log(humidity + "big humidity pooopy weather scum")
-    console.log("big poopy wind speed"+wind)
-    console.log("big poopy icon"+ icon)
+    icon = weatherData.weather[0].icon;
+    console.log("weatherData" + JSON.stringify(weatherData));
+    console.log("temp" + temp);
+    console.log( "humidity " +humidity);
+    console.log("wind " + wind);
+    console.log("icon " + icon);
 
-    function getweekweather() {
-       var lat = weatherData.coord.lat
-       var long = weatherData.coord.lon
-        
-        var apiTwo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`;
-        fetch(apiTwo).then(function (res) {
-            if (res.ok) {
-                res.json().then(function (weeklyData) {
-                    populateWeeklyData(weeklyData);
+// getting png for UI
+var weatherIcon = "http://openweathermap.org/img/w/"+icon+".png"
 
-                });
-            }
-        });
-    }
-    getweekweather();
+    // getting html ele
+    var tempRender = document.getElementById("temp");
+    var humRender = document.getElementById("humidity");
+    var windRender = document.getElementById("wind");
+    var ConidtionsRender = document.getElementById("imgCon");
+    var uvRender = document.getElementById("uv")
+
+    // render
+    tempRender.innerText = temp
+    humRender.innerText = humidity
+    windRender.innerText = wind
+    ConidtionsRender.src = weatherIcon
+
+
+
+
+    var lat = weatherData.coord.lat;
+    var long = weatherData.coord.lon;
+    console.log("I am lat " + lat);
+    console.log("I am long " + long);
+    // uses weatherdata lat and long to make 2nd request for week weather data
+    getweekweather(lat, long);
+}
+// makes req with 2nd api for weekly weather data this req uses lat and long from first req
+function getweekweather(lat, long) {
+    console.log(lat + " i am get week weather lat");
+    var apiTwo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`;
+    fetch(apiTwo).then(function (res) {
+        if (res.ok) {
+            res.json().then(function (weeklyData) {
+                JSON.stringify(weeklyData);
+                console.log("weekly weather data " + JSON.stringify(weeklyData));
+                // populateWeeklyData(weeklyData);
+            });
+        }
+    });
 }
 
-
-
-} 
-
-
-
-testData();
-
-// function makeRequest(endpoint) 
-//     return fetch(endpoint).then(function (res){
-//         return res.json();
-//     });
-// }
+searchFormEl.addEventListener("submit", citySearch);
 
 
 
 
-// var endpoint = generateEndpoint(city);
 
-// makeRequest(endpoint).then(function(weatherData){
 
-// };
